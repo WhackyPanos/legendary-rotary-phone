b1aab6210f7e86cd44464ab5957bab8676194656
BuildTools <unconfigured@null.spigotmc.org>
2020-05-19 19:20:10 +0300

    -added a check for diversity between rounds -100% done!!!!!

--- Delphi/delphi method.py
+++ Delphi/delphi method.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import os
 
 oldanswrs = ["nothing old yet"]
 answrs = []
-diafora = []
+pinakas_diaforon = ['There is no diversity yet']
 agrmnt = False
 
 while agrmnt == False:
@@ -10,6 +11,8 @@ while agrmnt == False:
     data = pd.read_excel(r'C:\Users\Hristos Birbou\PycharmProjects\legendary-rotary-phone\Delphi\data.xlsx', index_col=0)
 
     r, c = data.shape
+    diafora = 0
+    sumfonia = 0
 
 
     for i in range(r):
@@ -24,17 +27,32 @@ while agrmnt == False:
         answrs.append(motemp)
 
         if oldanswrs[0] != "nothing old yet":
-            diafora.append(oldanswrs[i] - answrs[i])
+            diafora = oldanswrs[i] - answrs[i]
+            pinakas_diaforon.append(abs(round(diafora,2)))
+
+            if pinakas_diaforon[i] < 0.05:
+                sumfonia += 1
 
 
-    print(len(answrs), answrs, oldanswrs)
-    print(diafora)
+    #print(len(answrs), answrs, oldanswrs)
+    print('----------ANSWERS----------')
+    print('-The answers from this round are:', answrs)
+    print('-The answers from the previous round are:', oldanswrs)
+    print('-The diversity between the last two rounds is:', pinakas_diaforon)
+
     oldanswrs.clear()
     oldanswrs = answrs.copy()
     answrs.clear()
-    diafora.clear()
+    pinakas_diaforon.clear()
 
-    apantisi = input('ok?')
-
-    if apantisi == 'ok':
+    if sumfonia == r:
         agrmnt = True
+
+    else:
+        input('Press enter to continue...')
+
+
+print("--------------------The weights are--------------------")
+
+for i in range(r):
+    print('Er', i, ': ', round(oldanswrs[i], 2), sep='')
